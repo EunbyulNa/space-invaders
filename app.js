62af5d67edd87ca60b81814e5e47b6bc47ffc410
Eunbyul <las4134@gmail.com>
2023-02-13 12:56:01 +0000

    Add spaceship keydown function

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ let spaceship = document.querySelector("#spaceship");
 
 
 window.addEventListener("keydown", function(e) {
+  console.log(e)
   let left = parseInt(window.getComputedStyle(spaceship).getPropertyValue("left"));
-  if(e.key = "ArrowLeft") {
-    spaceship.style.left = left - 10 + "px"
-  };
+  let top = parseInt(spaceship.offsetTop);
+  console.log(top)
+  if(e.key == "ArrowLeft" && left > 0) {
+     spaceship.style.left = left - 10 + "px"
+  } else if(e.key == "ArrowRight" && left < 540){
+    spaceship.style.left = left + 10 + "px"
+  } else if( e.key == "ArrowUp" && top > 36){
+    spaceship.style.top = top - 20 + "px"
+  } else if( e.key == "ArrowDown" && top < 520){
+      spaceship.style.top = top + 20 + "px"
+  }
 });
