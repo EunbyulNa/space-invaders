bb8f109129eec49dcc66d18feb7e34bf3e99176e
Eunbyul <las4134@gmail.com>
2023-02-15 15:17:16 +0000

    Create start img, and keydown event

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-let spaceship = document.querySelector("#spaceship");
 
 const container= document.querySelector(".container");
 
+window.addEventListener("DOMContentLoaded", function () {
+  let start = document.createElement("div");
+  let startImg = document.createElement("IMG");
+  start.classList.add("start")
+  startImg.classList.add("start-img");
+  startImg.setAttribute("src", "img/start.jpeg");
+  container.appendChild(start);
+  start.appendChild(startImg);
+});
+
+window.addEventListener("keydown", function (e) {
+  let start = document.querySelector(".start")
+  if(e.code === "Enter") {
+     start.style.opacity = 0;
+     setTimeout(() => {
+  container.removeChild(start);
+  setTimeout(() => {
+    generateAlien();
+  }, 1000);
+}, 5000);
+
+  }
+})
+
+let spaceship = document.querySelector("#spaceship");
 //Spaceship position movement
 window.addEventListener("keydown", function(e) {
 
   //Get the left and top position
   let left = parseInt(window.getComputedStyle(spaceship).getPropertyValue("left"));
-  let top = parseInt(spaceship.offsetTop);
+  let top = parseInt(window.getComputedStyle(spaceship).getPropertyValue("top"));
+
+
 
    //If a left key is press down
    //Change the left position
@@ -28,21 +54,37 @@ window.addEventListener("keydown", function(e) {
        }
       break;
     case "ArrowDown":
-         if(top < 500){
+         if(top < 600){
         spaceship.style.top = top + 20 + "px"
      }
      break;
-    case "Space":
-    let bullet = document.createElement("IMG");
-    bullet.setAttribute("id", "bullet");
-    bullet.setAttribute("src", "img/bullet.png");
-      container.appendChild(bullet);
-     break;
+
    }
 });
 
 
+window.addEventListener("keydown", function (e) {
+  if(e.keyCode == 32) {
+
+    let bullet = document.createElement("IMG");
+    bullet.setAttribute("id", "bullet");
+    bullet.setAttribute("src", "img/bullet.png");
+    container.appendChild(bullet);
+
+    //Updating bullet bottom position every seconds
+    /*let moveBullet = setInterval(() => {
+      let bulletBottom =  parseInt(window.getComputedStyle(bullet).getPropertyValue("bottom"));
+      console.log(bulletBottom)
+     bullet.style.bottom = bulletBottom + 3 + 'px'
+   },2000)*/
+ }
+})
+
 //Generate aliens
+
+
+
+
 function generateAlien() {
 
 const alien = document.createElement("IMG");
@@ -52,6 +94,9 @@ const alien = document.createElement("IMG");
 alien.style.left = Math.floor(Math.random() * 1000)+ 'px';
 container.appendChild(alien);
 };
+
+
+
 
 //Move down aliens
 function moveAlien() {
@@ -65,5 +110,5 @@ function moveAlien() {
   });
 };
 
-//setInterval(generateAlien,1500);
-//setInterval(moveAlien,450);
+setInterval(generateAlien,1500);
+setInterval(moveAlien,450);
